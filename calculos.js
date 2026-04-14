025751c29667127d85039b6f7179cffd45582069
TomasErnestoN <tomas_e_carvalho@estudante.sesisenai.org.br>
2026-04-14 19:40:40 -0300

    adicionando testes e organizando o visual do html

--- calculos.js
+++ calculos.js
@@ -1,44 +1,32 @@
-export function soma(n1, n2){
-    return n1 + n2
+export function soma(a, b) {
+    return a + b;
 }
 
-export function subtracao(n1, n2){
-    return n1 - n2
+export function subtracao(a, b) {
+    return a - b;
 }
 
-export function multiplicacao(n1, n2){
-    return n1 * n2
+export function multiplicacao(a, b) {
+    return a * b;
 }
 
-export function divisao(n1, n2){
-    return n1 / n2
+export function divisao(a, b) {
+    return a / b;
 }
 
-
-export function exponenciacao(n1){
-    return n1 ** 2
+export function exponenciacao(a) {
+    return a * a;
 }
 
-export function raiz(n1){
-    return Math.sqrt(n1)
+export function raiz(a) {
+    return Math.sqrt(a);
 }
 
-export function fatorial(n){
-
-if (n < 0) {
-    return "Erro: número negativo"
-}
-
-if (!Number.isInteger(n)) {
-    return "Erro: use inteiro"
-}
-
-let resultado = 1
-
-for(let i = 1; i <= n; i++){
-    resultado *= i
-}
-
-return resultado
-
+export function fatorial(n) {
+    if (n < 0) return NaN;
+    let r = 1;
+    for (let i = 1; i <= n; i++) {
+        r *= i;
+    }
+    return r;
 }
