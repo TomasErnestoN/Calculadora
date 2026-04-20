9e5938ea2593169cdb75cdf8505955100ed6c7d1
Kaio-dev2 <kaio_r_lisboa@estudante.sesisenai.org.br>
2026-04-20 17:33:22 -0300

    detalhes

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // Define a URL base para que no teste você use apenas cy.visit('/')
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:5500/public/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // Garante que o robô não desista rápido se o site demorar a carregar
-    defaultCommandTimeout: 10000, 
   },
 });
